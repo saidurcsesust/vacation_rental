635a4c714d4bc1573068000c0d53f0b860eb44f6
saidurcsesust <saidur.cse.sust19@gmail.com>
2026-02-10 10:46:03 +0600

    removed some files

--- backend/properties/admin.py
+++ backend/properties/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Image, Location, Property
 
@@ -17,12 +18,20 @@ class LocationAdmin(admin.ModelAdmin):
 
 @admin.register(Property)
 class PropertyAdmin(admin.ModelAdmin):
-    list_display = ('title', 'location', 'price_per_night', 'is_active', 'updated_at')
+    list_display = ('title', 'location', 'price_per_night', 'is_active', 'updated_at', 'upload_image')
     list_filter = ('is_active', 'location')
     list_select_related = ('location',)
     prepopulated_fields = {'slug': ('title',)}
     search_fields = ('title', 'slug', 'location__name')
     inlines = [ImageInline]
+
+    def upload_image(self, obj):
+        return format_html(
+            '<a href="/admin/properties/image/add/?property={}">Upload image</a>',
+            obj.pk,
+        )
+
+    upload_image.short_description = 'Image Upload'
 
 
 @admin.register(Image)
@@ -31,3 +40,23 @@ class ImageAdmin(admin.ModelAdmin):
     list_filter = ('is_primary',)
     list_select_related = ('property',)
     search_fields = ('property__title', 'alt_text', 'image_url')
+
+    def get_changeform_initial_data(self, request):
+        initial = super().get_changeform_initial_data(request)
+        property_id = request.GET.get('property')
+        if property_id:
+            initial['property'] = property_id
+        return initial
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        property_id = request.GET.get('property')
+        if obj is None and property_id and 'property' in form.base_fields:
+            form.base_fields['property'].disabled = True
+        return form
+
+    def save_model(self, request, obj, form, change):
+        property_id = request.GET.get('property')
+        if not change and property_id:
+            obj.property_id = property_id
+        super().save_model(request, obj, form, change)
